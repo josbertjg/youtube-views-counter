623ce713a9a110854de0c473e4a771982c08f268
ReguloCH <regulochourio4@gmail.com>
2025-01-25 18:44:46 -0400

    Edit index.js

--- js/index.js
+++ js/index.js
@@ -1,44 +1,53 @@
-$(document).ready(()=>{
+$(document).ready(() => {
   let countQ = 0;
   let countW = 0;
   let countE = 0;
   let countR = 0;
   let keyPressed = null;
+
   const channelName = $("#channel-name");
   const videoName = $("#video-name");
   const videoDescription = $("#video-description");
   const infoModal = new bootstrap.Modal(document.getElementById("info-modal"));
   const finishTransmision = new bootstrap.Modal(document.getElementById("finish-modal"));
 
-  // Mostrando el modal a penas carga la ventana
-  infoModal.show()
+  // Mostrar modal al cargar la ventana
+  infoModal.show();
 
-  // Si existen valores en el local storage, entonces se setea en los inputs
+  // Recuperar estado inicial
   setState();
-  
-  // Eventos de la ventana
-  window.onkeydown = (event) => keyPressed = event.key;
-  
+
+  // Evento de teclado
+  window.onkeydown = (event) => (keyPressed = event.key);
+
+  // Evento de scroll optimizado para manejar likes y dislikes
   window.onwheel = (event) => {
-    const direction = Math.sign(event.deltaY) < 0
+    const direction = Math.sign(event.deltaY) < 0;
+    const counters = { countQ, countW, countE, countR };
 
-    if(keyPressed == 'q') countQ = updateCounter(countQ,direction,6)
-    if(keyPressed == 'w') countW = updateCounter(countW,direction,12)
-    if(keyPressed == 'e') countE = updateCounter(countE,direction,12)
-    if(keyPressed == 'r') countR = updateCounter(countR,direction,132)
+    // Handle likes and dislikes with the scroll
+    handleDislikesAndLikes(counters, keyPressed, direction);
 
-    updateViewContent({countQ, countW, countE, countR})
-  }
+    // Update global counters
+    countQ = counters.countQ;
+    countW = counters.countW;
+    countE = counters.countE;
+    countR = counters.countR;
+  };
 
-  // Eventos de botones de accion
-  $(".btn-info").click(()=>infoModal.show())
-  $(".btn-finish").click(()=>finishTransmision.show())
+  // Botones de acción
+  $(".btn-info").click(() => infoModal.show());
+  $(".btn-finish").click(() => finishTransmision.show());
 
-  // Eventos del modal
-  $("#btn-accept").click(()=>{
-    if(channelName.val().trim().length != 0 && videoName.val().trim().length != 0 && videoDescription.val().trim().length != 0){
+  // Modal de información
+  $("#btn-accept").click(() => {
+    if (
+      channelName.val().trim().length !== 0 &&
+      videoName.val().trim().length !== 0 &&
+      videoDescription.val().trim().length !== 0
+    ) {
       infoModal.hide();
-      saveState(channelName.val(),videoName.val(),videoDescription.val())
+      saveState(channelName.val(), videoName.val(), videoDescription.val());
     }
-  })
-})
+  });
+});
